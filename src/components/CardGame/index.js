1d909a2070fd0fc880c5d43c2fc738142b6f3e3f
Hyago Brito <110845341+Hyagodevbfr@users.noreply.github.com>
2022-09-23 10:04:00 -0300

    Add flip CardGame

--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -2,7 +2,7 @@ import './style.css';
 
 function CardGame(icon = 'alura-pixel', alt = 'Logo da Alura'){
     return /*html*/`
-    <article id="CardGame">
+    <article id="card-game">
         <img src="src/images/${icon}.svg" alt="${alt}">
     </article>
     `
