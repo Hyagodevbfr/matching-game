950bfff2fa023d00c15f905a2bb98373e95860de
Hyago Brito <110845341+Hyagodevbfr@users.noreply.github.com>
2022-09-27 16:16:48 -0300

    Swap player when flip to card

--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -3,11 +3,35 @@ import CardFrontBack from '../../components/CardFrontBack';
 import cards from './data.js'
 
 function BoardGame(amoutCards){
+  const FlipAndHideCards = ($cardsActive) => {
+    $cardsActive.forEach((card) => card.classList.remove('-active'))
+  }
+  const swapPlayer = ($arrowDown) =>{
+    const currentPlayer = $arrowDown.getAttribute('data-currentplayer');
+
+    $arrowDown.setAttribute('data-currentPlayer', currentPlayer == 1 ? 2 : 1);
+  }
+  window.boardGame = {};
+  window.boardGame.handleClick = () =>{
+    const $boardGame = document.querySelector('.board-game');
+    const $arrowDown = document.querySelector('.arrow-down');
+    const $cardsActive = $boardGame.querySelectorAll('.card-front-back.-active')
+
+    if ($cardsActive.length == 2){
+      setTimeout(() =>{
+        
+        FlipAndHideCards($cardsActive)
+        swapPlayer($arrowDown)
+      }, 1000);
+
+      
+    }
+  }
     const $htmlCardsList = cards.map((card) => CardFrontBack(card.icon, card.altIcon))
     const $htmlCards = $htmlCardsList.join('')
 
     return /*html*/`
-      <section class="board-game">
+      <section class="board-game" onclick="boardGame.handleClick()">
       ${/*htmlContent*/ $htmlCards}
       ${/*htmlContent*/ $htmlCards}
       </section>
